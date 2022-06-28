3a1528262f5614bedc9099a98494746a118c4bb0
Crowdbotics <engineering@crowdbotics.com>
2022-06-28 17:11:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "melv_rapidproto_tra_25183",
+  url: "https://melv_rapidproto_tra_25183.botics.co",
+  api: "https://melv_rapidproto_tra_25183.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
